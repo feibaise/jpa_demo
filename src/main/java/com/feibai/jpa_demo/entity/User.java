a0d305de914451b07f5a3d6b3300127ed15b040f
feibai <wushi88@vip.qq.com>
2019-01-28 10:01:55 +0800

    新建实体

---
+++ src/main/java/com/feibai/jpa_demo/entity/User.java
@@ -0,0 +1,11 @@
+package com.feibai.jpa_demo.entity;
+
+/**
+ * @Author: ${user}
+ * @description:
+ * @Date: Created in 10:01 2019/1/28
+ * @Modfied by: ${user}
+ * @Modfied Date by: 10:01 2019/1/28
+ */
+public class User {
+}
